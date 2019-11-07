92f4d5149e233ee33efbc3aa773c2998b3b1073f
Arib Alam <aribalam64@gmail.com>
2019-11-07 23:05:45 +0530

    removed old files and added react package

---
+++ friday/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import TextView from './ui/textEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <TextView />
+    </div>
+  );
+}
+
+export default App;
