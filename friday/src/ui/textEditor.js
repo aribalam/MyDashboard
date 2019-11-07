91a08d3d920a1232dda250e2a4baac73815acc77
Arib Alam <aribalam64@gmail.com>
2019-11-07 20:29:17 +0530

    created component textView

--- friday/src/ui/textEditor.js
+++ friday/src/ui/textEditor.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function textView(props) {
+export default function TextView(props) {
+
+    const [placeholder, setPlaceholder] = useState(true);
+
+    const handleChange = (event) => {
+        if (event.target.innerHTML.length == 1 && event.key === "Backspace")
+            setPlaceholder(true);
+        else
+            setPlaceholder(false);
+    }
 
     return (
-        <input type='text' className="text-input"></input>
+        <div className="text-wrapper" style={{fontSize: props.fontSize}}>
+            <div className="text-input" contentEditable="true" onKeyDown={handleChange}>
+            </div>
+            {   placeholder? 
+                <div className="placeholder-overlay">Title..</div> : ''
+            }
+        </div>
     )
 
 }
