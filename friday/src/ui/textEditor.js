e1cfedf87567b77a081c33ccc5340f40f441879a
Arib Alam <aribalam64@gmail.com>
2019-11-12 15:07:35 +0530

    make content not editable on enter

--- friday/src/ui/textEditor.js
+++ friday/src/ui/textEditor.js
@@ -3,18 +3,25 @@ import React, {useState} from 'react'
 export default function TextView(props) {
 
     const [placeholder, setPlaceholder] = useState(true);
+    const [editable, setEditable] = useState(true);
 
     const handleChange = (event) => {
-        if (event.target.innerHTML.length == 1 && event.key === "Backspace")
+        var text = event.target.innerHTML;
+        if (text.length < 1)
+            setPlaceholder(true);
+        else if (text.length == 1 && event.key === "Backspace")
             setPlaceholder(true);
         else
             setPlaceholder(false);
+        
+        if (text.length > 0 && event.key === "Enter")
+            setEditable(false);
     }
 
     return (
         <div className="text-wrapper" style={{fontSize: props.fontSize}}>
-            <div className="text-input" contentEditable="true" onKeyDown={handleChange}>
-            </div>
+            <p className="text-input" contentEditable={editable} onKeyDown={handleChange}>
+            </p>
             {   placeholder? 
                 <div className="placeholder-overlay">Title..</div> : ''
             }
